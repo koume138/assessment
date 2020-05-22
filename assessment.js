e9a09400bb6aade55d09fffb8845dc74a9c5dd53
koume138 <62248286+koume138@users.noreply.github.com>
2020-05-22 15:36:47 +0900

    Update assessment.js

--- assessment.js
+++ assessment.js
@@ -53,6 +53,7 @@ assessmentButton.onclick = () => {
   anchor.className = 'twitter-hashtag-button';
   anchor.setAttribute('data-text', result);
   anchor.innerText = 'Tweet #あなたのいいところ';
+  
   tweetDivided.appendChild(anchor);
 
   // widgets.js の設定
